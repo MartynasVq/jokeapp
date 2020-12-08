5a39c08e7a85462dea464e36a6a6ee2331d818bc
Martynas <nbtsst@gmail.com>
2020-12-08 16:53:39 +0200

    working app

---
+++ src/main/java/com/learn/jokeapp/controller/JokeController.java
@@ -0,0 +1,24 @@
+package com.learn.jokeapp.controller;
+
+import com.learn.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService js) {
+        this.jokeService = js;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/com/learn/jokeapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.learn.jokeapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
